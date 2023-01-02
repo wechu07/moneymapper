9c16b3ff011c48c5db1294ece5fd2491eb47e42b
wechuli07 <wechuli017@gmail.com>
2023-01-02 17:44:34 +0300

    config server connect to sqlite db

--- config/db.js
+++ config/db.js
@@ -1,45 +1,8 @@
-const sqlite3 = require('sqlite3');
+const { Sequelize } = require('sequelize')
 
-module.exports = function configDB() {
-  const db = new sqlite3.Database('moneymapper.db');
+const sequelize = new Sequelize('moneymapper-db', 'user', 'password', {
+  dialect: 'sqlite',
+  host: './moneymapper.sqlite'
+})
 
-  db.serialize(() => {
-    db.run(`
-      CREATE TABLE IF NOT EXISTS payments (
-        id INTEGER PRIMARY KEY,
-        amount REAL NOT NULL,
-        paid_at DATETIME NOT NULL,
-        user_id INTEGER NOT NULL,
-        mpesa_transaction_id TEXT NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users(id)
-      )
-    `);
-
-    db.run(`
-      CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL
-      )
-    `);
-  });
-
-  return {
-    getPaymentsByPeriod: (start, end, callback) => {
-      db.all(`
-        SELECT users.name, SUM(payments.amount) as total_paid
-        FROM users
-        JOIN payments ON payments.user_id = users.id
-        WHERE payments.paid_at BETWEEN ? AND ?
-        GROUP BY users.id
-      `, [start, end], (err, rows) => {
-        if (err) {
-          console.error(err.message);
-          callback(err);
-        } else {
-          callback(null, rows);
-        }
-      });
-    }
-  };
-};
+module.exports = sequelize
